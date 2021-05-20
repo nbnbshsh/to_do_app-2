7eafbe8a97fa2c47923f0a44d01ca9352c6180ad
nbnbshsh <hikonyanwan@hotmail.co.jp>
2021-05-21 02:03:51 +0900

    JS　読み込み修正

---
+++ app/assets/javascripts/reviews.js
@@ -0,0 +1,18 @@
+const stars = document.querySelector(".ratings").children;
+const ratingValue = document.getElementById("rating-value");
+
+let index;
+
+for(let i=0;i<stars.length; i++){
+  stars[i].addEventListner("mouseover",function(){
+    for(let j=0; j<stars.lemgth; j++){
+    stars[j].classList.remove("fa-star");
+    stars[j].classList.add("fa-star-o");
+  }
+  
+    for(let j=0; j<=i; j++){
+    stars[j].classList.remove("fa-star-o");
+    stars[j].classList.add("fa-star");
+  }
+  })
+}
